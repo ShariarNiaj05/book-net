9d47e440c843cc4d2dc1a5b72e6a9d43608e7033
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-08 21:57:12 +0600

    banner added

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,10 +2,9 @@ import Banner from "@/components/banner/Banner";
 
 const HomePage = () => {
   return (
-    <div className="">
+    <>
       <Banner />
-      {/* <h1 className="text-4xl">Welcome To Book Net Home Page</h1> */}
-    </div>
+    </>
   );
 };
 
