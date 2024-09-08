25a65c44c427ab8ab0c541c4fc8f5e8c61c5f8a8
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-09 00:57:12 +0600

    comment service

---
+++ src/services/commentServices/index.ts
@@ -0,0 +1,33 @@
+import { delay } from "@/utils/delay";
+
+export const getAllComments = async (
+  id: string,
+  type?: string,
+  wait = false
+) => {
+  let fetchOptions = {};
+
+  if (type === "ssr") {
+    fetchOptions = {
+      cache: "no-store",
+    };
+  } else if (type === "isr") {
+    fetchOptions = {
+      next: {
+        revalidate: 30,
+      },
+    };
+  }
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API}/posts`,
+    fetchOptions
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts data");
+  }
+  if (wait) {
+    delay(2000);
+  }
+  return res.json();
+};
