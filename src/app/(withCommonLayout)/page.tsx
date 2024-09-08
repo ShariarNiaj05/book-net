f15c513c245c3aa8a99e4b22448f10787acaa8fb
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-08 21:56:47 +0600

    banner added

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,7 +2,7 @@ import Banner from "@/components/banner/Banner";
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="">
       <Banner />
       {/* <h1 className="text-4xl">Welcome To Book Net Home Page</h1> */}
     </div>
