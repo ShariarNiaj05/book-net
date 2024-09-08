e5a777656bf4f142289eb5161cdaeb91d4862d72
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-09 00:18:47 +0600

    post services

--- src/services/postServices/index.ts
+++ src/services/postServices/index.ts
@@ -1,7 +1,12 @@
 export const getAllPosts = async (type: string) => {
-  let fetchOptions = {
-    cache: type,
-  };
+  let fetchOptions = {};
+
+  if (type === "ssr") {
+    fetchOptions = {
+      cache: "no-store",
+    };
+  }
+
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_API}/posts`,
     fetchOptions
