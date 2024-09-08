d6c58be62493210d431175fbce0250a73fd63174
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-09 01:43:46 +0600

    dashboard layout

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -1,7 +1,8 @@
+import Sidebar from "@/components/ui/Sidebar";
 import { ReactNode } from "react";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
+  return <Sidebar>{children}</Sidebar>;
 };
 
 export default DashboardLayout;
