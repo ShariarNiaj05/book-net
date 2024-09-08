41b7cf29c80fd5e77d87c09ecea5649e95f6a18c
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-09 01:47:00 +0600

    dashboard layout

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = ({ children }: { children: ReactNode }) => {
         ></label>
         <ul className="menu bg-fuchsia-100 text-base-content min-h-full w-80 p-4">
           {/* Sidebar content here */}
-          <li className="bg-fuchsia-200 rounded">
+          <li className="bg-fuchsia-200 rounded mb-2">
             <Link href={"/"}>Home</Link>
           </li>
           <li className="bg-fuchsia-200 rounded">
