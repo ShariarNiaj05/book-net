8f5a3744602ca8d8625c257d0b29139e6f393baa
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-09 00:58:20 +0600

    comment service

--- src/services/commentServices/index.ts
+++ src/services/commentServices/index.ts
@@ -1,28 +1,8 @@
 import { delay } from "@/utils/delay";
 
-export const getAllComments = async (
-  id: string,
-  type?: string,
-  wait = false
-) => {
-  let fetchOptions = {};
+export const getCommentsById = async (id: string, wait = false) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts`);
 
-  if (type === "ssr") {
-    fetchOptions = {
-      cache: "no-store",
-    };
-  } else if (type === "isr") {
-    fetchOptions = {
-      next: {
-        revalidate: 30,
-      },
-    };
-  }
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_API}/posts`,
-    fetchOptions
-  );
   if (!res.ok) {
     throw new Error("Failed to fetch posts data");
   }
