0b358617d35d771bbf798de8cbdd2604d88acd4e
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-08 23:59:09 +0600

    post services

--- src/services/postServices/index.ts
+++ src/services/postServices/index.ts
@@ -1,5 +1,7 @@
 export const getAllPosts = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts`, {
+    cache: "no-store",
+  });
   if (!res.ok) {
     throw new Error("Failed to fetch posts data");
   }
