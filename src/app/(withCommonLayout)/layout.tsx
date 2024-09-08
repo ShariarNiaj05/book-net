f8a224f2368d7592f10b0881a81745d6f99a763a
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-08 20:14:33 +0600

    layout

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from "react";
 
 const CommonLayout = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <h1>Header</h1>
+      {children}
+      <h2>Footer</h2>
+    </>
+  );
 };
 
 export default CommonLayout;
